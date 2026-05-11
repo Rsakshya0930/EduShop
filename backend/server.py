16194b6a4e88df7c277633a2c8e0d38c41ed5d85
Rsakshya0930 <sakshyaraghav09@gmail.com>
2026-05-12 03:01:44 +0530

    final cors

--- backend/server.py
+++ backend/server.py
@@ -238,7 +238,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 origins = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "https://edushop-frontend.onrender.com",
 ]
 
 app.add_middleware(
